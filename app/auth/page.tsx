d49858c6bf90ba6f84cbeb42bba80a095d09aa25
dejvidEm <bithug10@gmail.com>
2024-09-21 21:59:22 +0200

    register page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -3,23 +3,31 @@ import React from 'react'
 const page = () => {
   return (
     <div className='w-full h-screen flex items-center justify-center bg-cover bg-center p-10' style={{ backgroundImage: "url('/images/bg-auth.jpg')" }}>
-      <div className='bg-white bg-opacity-10 backdrop-blur-md p-8 rounded-[10px] shadow-lg'>
+      <div className='bg-white bg-opacity-10 backdrop-blur-md pb-8 w-[450px] px-8 rounded-[10px] shadow-lg'>
+        <div>
+        <h1 className='flex justify-center text-xl text-gray-200 p-6 font-bold'>Login Page</h1>
+        </div>
         <form action="">
           <div className='pb-2'>
-            <h1 className='text-gray-200 pt-2 px-2 font-bold'>Login</h1>
-            <input type="text" className='rounded-[10px] pl-4 h-10'/>
+            <h1 className='text-gray-200 pt-2 px-2'>E-mail</h1>
+            <input type="text" className='pl-4 pb-1 caret-white outline-none h-10 border-b border-gray-300 bg-transparent text-gray-300 w-full'/>
           </div>
           <div className='py-4'>
-            <h1 className='text-gray-200 pt-2 px-2 font-bold'>Password</h1>
-            <input type="text" className='rounded-[10px] pl-4 h-10'/>
+            <h1 className='text-gray-200 pt-2 px-2'>Password</h1>
+            <input type="text" className='pl-4 pb-1 caret-white outline-none h-10 border-b border-gray-300 bg-transparent text-gray-300 w-full'/>
           </div>
-          <div className='flex flex-row justify-between'>
-            <h1 className='text-gray-900 font-bold p-2'>Register</h1>
-            <button className="before:ease relative font-semibold rounded-[10px] h-10 px-6 overflow-hidden border border-black shadow-2xl before:absolute before:left-0 before:-ml-2 before:h-48 before:w-48 before:origin-top-right before:-translate-x-full before:translate-y-12 before:-rotate-90 before:bg-gray-900 before:transition-all before:duration-300 hover:text-white hover:shadow-black hover:before:-rotate-180">
+          <div className='flex flex-row justify-between py-4'>
+            <button className="before:ease relative font-semibold rounded-[10px] h-10 w-full overflow-hidden border border-black shadow-2xl before:absolute before:left-0 before:h-48 before:w-full before:origin-top-right before:-translate-x-full before:translate-y-12 before:-rotate-90 before:bg-gray-900 before:transition-all before:duration-300 hover:text-white hover:shadow-black hover:before:-rotate-180">
               <span className="relative z-10">Log In</span>
             </button>
           </div>
         </form>
+        <div className='flex justify-between flex-row'>
+          <p>No Account?</p>
+          <a href="/register">
+            <p className='text-emerald-500 font-medium'>Sign Up</p>
+          </a>
+        </div>
       </div>
     </div>
   )
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,7 +2,39 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='w-full h-screen flex items-center justify-center bg-cover bg-center p-10' style={{ backgroundImage: "url('/images/bg-auth.jpg')" }}>
+      <div className='bg-white bg-opacity-10 backdrop-blur-md pb-8 px-8 w-[450px] rounded-[10px] shadow-lg'>
+        <div>
+        <h1 className='flex justify-center text-xl text-gray-200 p-6 font-bold'>Sign Up Page</h1>
+        </div>
+        <form action="">
+          <div className='pb-2'>
+            <h1 className='text-gray-200 pt-2 px-2'>Full Name</h1>
+            <input type="text" className='pl-4 pb-1 caret-white outline-none h-10 border-b border-gray-300 bg-transparent text-gray-300 w-full'/>
+          </div><div className='pb-2'>
+            <h1 className='text-gray-200 pt-2 px-2'>E-mail</h1>
+            <input type="text" className='pl-4 pb-1 caret-white outline-none h-10 border-b border-gray-300 bg-transparent text-gray-300 w-full'/>
+          </div>
+          <div className='py-4'>
+            <h1 className='text-gray-200 pt-2 px-2'>Password</h1>
+            <input type="text" className='pl-4 pb-1 caret-white outline-none h-10 border-b border-gray-300 bg-transparent text-gray-300 w-full'/>
+          </div>
+          <div className='flex gap-2'>
+            <input
+              className="h-4 w-4 text-green-800 focus:ring-green-800 border-gray-300 rounded"
+              type="checkbox"
+              id="rememberMe"
+            />
+            <p>I agree with <span className='text-emerald-700'>privacy</span> and <span className='text-emerald-700'>policy</span></p>
+          </div>
+          <div className='flex flex-row justify-between py-4'>
+            <button className="before:ease relative font-semibold rounded-[10px] h-10 w-full overflow-hidden border border-black shadow-2xl before:absolute before:left-0 before:h-48 before:w-full before:origin-top-right before:-translate-x-full before:translate-y-12 before:-rotate-90 before:bg-gray-900 before:transition-all before:duration-300 hover:text-white hover:shadow-black hover:before:-rotate-180">
+              <span className="relative z-10">Sign Up</span>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
