75484551efa3517c88dd25877f3a6cf32fdff47b
dejvidEm <bithug10@gmail.com>
2024-10-09 01:29:18 +0200

    updated responsitivity issue on auth and reg pages

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -2,38 +2,44 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='w-full h-screen flex flex-col gap-10 items-center justify-center bg-cover bg-center p-10' style={{ backgroundImage: "url('/images/bg-auth.jpg')" }}>
+    <div className='w-full h-screen flex flex-col gap-10 items-center justify-center bg-cover bg-center p-4 md:p-10' style={{ backgroundImage: "url('/images/bg-auth.jpg')" }}>
       <div>
-        <img src="images/tripple_logo.png" alt="logo" className='rounded-[50%] w-40'/>
+        <img src="images/tripple_logo.png" alt="logo" className='rounded-[50%] w-24 md:w-40' />
       </div>
-      <div className='bg-white bg-opacity-10 backdrop-blur-md pb-8 w-[450px] px-8 rounded-[10px] shadow-lg'>
+      <div className='bg-white bg-opacity-10 backdrop-blur-md pb-8 w-full max-w-md px-4 md:px-8 rounded-[10px] shadow-lg'>
         <div>
-        <h1 className='flex justify-center text-xl text-gray-200 p-6 font-bold'>Login Page</h1>
+          <h1 className='flex justify-center text-lg md:text-xl text-gray-200 p-4 md:p-6 font-bold'>Login Page</h1>
         </div>
         <form action="">
           <div className='pb-2'>
             <h1 className='text-gray-200 pt-2 px-2'>E-mail</h1>
-            <input type="text" className='pl-4 pb-1 caret-white outline-none h-10 border-b border-gray-300 bg-transparent text-gray-300 w-full'/>
+            <input 
+              type="text" 
+              className='pl-4 pb-1 caret-white outline-none h-10 border-b border-gray-300 bg-transparent text-gray-300 w-full' 
+            />
           </div>
           <div className='py-4'>
             <h1 className='text-gray-200 pt-2 px-2'>Password</h1>
-            <input type="text" className='pl-4 pb-1 caret-white outline-none h-10 border-b border-gray-300 bg-transparent text-gray-300 w-full'/>
+            <input 
+              type="password" 
+              className='pl-4 pb-1 caret-white outline-none h-10 border-b border-gray-300 bg-transparent text-gray-300 w-full' 
+            />
           </div>
           <div className='flex flex-row justify-between py-4'>
-            <button className="before:ease relative font-semibold rounded-[10px] h-10 w-full overflow-hidden border border-black shadow-2xl before:absolute before:left-0 before:h-48 before:w-full before:origin-top-right before:-translate-x-full before:translate-y-12 before:-rotate-90 before:bg-gray-900 before:transition-all before:duration-300 hover:text-white hover:shadow-black hover:before:-rotate-180">
+            <button className="relative font-semibold rounded-[10px] h-10 w-full overflow-hidden border border-black shadow-2xl before:absolute before:left-0 before:h-48 before:w-full before:origin-top-right before:-translate-x-full before:translate-y-12 before:-rotate-90 before:bg-gray-900 before:transition-all before:duration-300 hover:text-white hover:shadow-black hover:before:-rotate-180">
               <span className="relative z-10">Log In</span>
             </button>
           </div>
         </form>
-        <div className='flex justify-between flex-row'>
-          <p>No Account?</p>
+        <div className='flex justify-between items-center py-4 text-sm md:text-base'>
+          <p className='text-gray-300'>No Account?</p>
           <a href="/register">
             <p className='text-emerald-500 font-medium'>Sign Up</p>
           </a>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default page
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -6,8 +6,6 @@ const SignUpPage = () => {
   const [fullName, setFullName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const [successMessage, setSuccessMessage] = useState('');
 
   const handleSubmit = async (e: { preventDefault: () => void; }) => {
     e.preventDefault();
@@ -20,85 +18,78 @@ const SignUpPage = () => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          fullName,
-          email,
-          password,
+          fullName, 
+          email,   
+          password 
         }),
       });
 
-      // Kontrola odpovede
       if (response.ok) {
         const data = await response.json(); // Správne spracovanie JSON odpovede
-        setSuccessMessage(data.message); // Uloženie správy o úspechu
-        setErrorMessage(''); // Resetovanie chybovej správy
+        console.log(data);
       } else {
         const errorData = await response.json(); // Získanie chybových dát
-        setErrorMessage(errorData.error || 'Nastala chyba, skúste znova.'); // Zobrazenie chybovej správy
-        setSuccessMessage(''); // Resetovanie správy o úspechu
+        console.log(errorData);
       }
     } catch (error) {
       console.error('Chyba pri volaní API:', error);
-      setErrorMessage('Nastala chyba, skúste znova.');
     }
   };
 
   return (
-    <div className='w-full h-screen flex flex-col gap-10 items-center justify-center bg-cover bg-center p-10' style={{ backgroundImage: "url('/images/bg-auth.jpg')" }}>
-      <div>
-        <img src="images/tripple_logo.png" alt="logo" className='rounded-[50%] w-40' />
-      </div>
-      <div className='bg-white bg-opacity-10 backdrop-blur-md pb-8 px-8 w-[450px] rounded-[10px] shadow-lg'>
-        <div>
-          <h1 className='flex justify-center text-xl text-gray-200 p-6 font-bold'>Sign Up Page</h1>
-        </div>
-        <form onSubmit={handleSubmit}>
-          <div className='pb-2'>
-            <h1 className='text-gray-200 pt-2 px-2'>Full Name</h1>
-            <input 
-              type="text" 
-              value={fullName} 
-              onChange={(e) => setFullName(e.target.value)}
-              className='pl-4 pb-1 caret-white outline-none h-10 border-b border-gray-300 bg-transparent text-gray-300 w-full' 
-            />
-          </div>
-          <div className='pb-2'>
-            <h1 className='text-gray-200 pt-2 px-2'>E-mail</h1>
-            <input 
-              type="email" 
-              value={email} 
-              onChange={(e) => setEmail(e.target.value)}
-              className='pl-4 pb-1 caret-white outline-none h-10 border-b border-gray-300 bg-transparent text-gray-300 w-full' 
-            />
-          </div>
-          <div className='py-4'>
-            <h1 className='text-gray-200 pt-2 px-2'>Password</h1>
-            <input 
-              type="password" 
-              value={password} 
-              onChange={(e) => setPassword(e.target.value)}
-              className='pl-4 pb-1 caret-white outline-none h-10 border-b border-gray-300 bg-transparent text-gray-300 w-full' 
-            />
-          </div>
-          <div className='flex gap-2'>
-            <input
-              className="h-4 w-4 text-green-800 focus:ring-green-800 border-gray-300 rounded"
-              type="checkbox"
-              id="rememberMe"
-            />
-            <p>I agree with <span className='text-emerald-700'>privacy</span> and <span className='text-emerald-700'>policy</span></p>
-          </div>
-          <div className='flex flex-row justify-between py-4'>
-            <button type="submit" className="before:ease relative font-semibold rounded-[10px] h-10 w-full overflow-hidden border border-black shadow-2xl before:absolute before:left-0 before:h-48 before:w-full before:origin-top-right before:-translate-x-full before:translate-y-12 before:-rotate-90 before:bg-gray-900 before:transition-all before:duration-300 hover:text-white hover:shadow-black hover:before:-rotate-180">
-              <span className="relative z-10">Sign Up</span>
-            </button>
-          </div>
-        </form>
-        {/* Zobrazenie správy o úspechu alebo chybe */}
-        {successMessage && <p style={{ color: 'green' }}>{successMessage}</p>}
-        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-      </div>
+  <div className='w-full h-screen flex flex-col gap-10 items-center justify-center bg-cover bg-center p-4 md:p-10' style={{ backgroundImage: "url('/images/bg-auth.jpg')" }}>
+    <div>
+      <img src="images/tripple_logo.png" alt="logo" className='rounded-[50%] w-24 md:w-40' />
     </div>
-  );
+    <div className='bg-white bg-opacity-10 backdrop-blur-md pb-8 px-4 md:px-8 w-full max-w-md rounded-[10px] shadow-lg'>
+      <div>
+        <h1 className='flex justify-center text-lg md:text-xl text-gray-200 p-4 md:p-6 font-bold'>Sign Up Page</h1>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className='pb-2'>
+          <h1 className='text-gray-200 pt-2 px-2'>Full Name</h1>
+          <input
+            type="text"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+            className='pl-4 pb-1 caret-white outline-none h-10 border-b border-gray-300 bg-transparent text-gray-300 w-full'
+          />
+        </div>
+        <div className='pb-2'>
+          <h1 className='text-gray-200 pt-2 px-2'>E-mail</h1>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className='pl-4 pb-1 caret-white outline-none h-10 border-b border-gray-300 bg-transparent text-gray-300 w-full'
+          />
+        </div>
+        <div className='py-4'>
+          <h1 className='text-gray-200 pt-2 px-2'>Password</h1>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className='pl-4 pb-1 caret-white outline-none h-10 border-b border-gray-300 bg-transparent text-gray-300 w-full'
+          />
+        </div>
+        <div className='flex gap-2'>
+          <input
+            className="h-4 w-4 text-green-800 focus:ring-green-800 border-gray-300 rounded"
+            type="checkbox"
+            id="rememberMe"
+          />
+          <p className='text-gray-300 text-sm'>I agree with <span className='text-emerald-700'>privacy</span> and <span className='text-emerald-700'>policy</span></p>
+        </div>
+        <div className='flex flex-row justify-between py-4'>
+          <button type="submit" className="relative font-semibold rounded-[10px] h-10 w-full overflow-hidden border border-black shadow-2xl before:absolute before:left-0 before:h-48 before:w-full before:origin-top-right before:-translate-x-full before:translate-y-12 before:-rotate-90 before:bg-gray-900 before:transition-all before:duration-300 hover:text-white hover:shadow-black hover:before:-rotate-180">
+            <span className="relative z-10">Sign Up</span>
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+);
 };
 
 export default SignUpPage;
